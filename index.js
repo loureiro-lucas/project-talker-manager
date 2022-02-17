a3ec842a3e00e2e40dee5642aa7e907b6f23f2b4
loureiro-lucas <lucas-loureiro@outlook.com>
2022-02-16 22:17:49 -0300

    requirement 2 done

--- index.js
+++ index.js
@@ -3,9 +3,11 @@ const bodyParser = require('body-parser');
 const fs = require('fs').promises;
 
 const app = express();
+
 app.use(bodyParser.json());
 
 const HTTP_OK_STATUS = 200;
+const HTTP_NOT_FOUND_STATUS = 404;
 const PORT = '3000';
 
 // não remova esse endpoint, e para o avaliador funcionar
@@ -15,8 +17,19 @@ app.get('/', (_request, response) => {
 
 app.get('/talker', async (_req, res, _next) => {
   const talkers = await fs.readFile('./talker.json', 'utf-8');
-  const talkersParsed = await JSON.parse(talkers);
-  return res.status(HTTP_OK_STATUS).send(talkersParsed);
+  const talkersParsed = JSON.parse(talkers);
+  return res.status(HTTP_OK_STATUS).json(talkersParsed);
+});
+
+app.get('/talker/:id', async (req, res, _next) => {
+  const { id } = req.params;
+  const talkers = await fs.readFile('./talker.json', 'utf-8');
+  const talkersParsed = JSON.parse(talkers);
+  const talker = talkersParsed.find((person) => person.id === Number(id));
+  if (!talker) {
+    return res.status(HTTP_NOT_FOUND_STATUS).json({ message: 'Pessoa palestrante não encontrada' });
+  }
+  return res.status(HTTP_OK_STATUS).json(talker);
 });
 
 app.listen(PORT, () => {
